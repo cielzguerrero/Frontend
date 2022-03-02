18c70410a371d805102849d5c73eb5a44a0f1b0b
aldrin24 <ramoresaldrinpup@gmail.com>
2022-03-03 01:54:00 +0800

    xd

--- index.php
+++ index.php
@@ -9,10 +9,17 @@ include('connections/connection.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<!-- MYCSS -->
+	
 	<link href="css/login1.css" rel="stylesheet" type="text/css" /></link>
 	<link href="css/style.css" rel="stylesheet" type="text/css" /></link>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- BOOTSTRAP JS -->
+	
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<!-- JS -->
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
@@ -47,8 +54,12 @@ include('connections/connection.php');
 			<div class="hero-col1">
 				<h1>SEGREGATE WASTE<br> EARN POINTS<br> CLAIM PRIZES!!!</h1>
 				<div class="hero-btn">
-				<input type="button" value="SIGN IN" class="btn1" ></input>
-				
+				<button type="button" class="btn2" data-toggle="modal" data-target="#SignInModal">
+				SIGN IN
+				</button>
+				<button type="button" class="btn2" data-toggle="modal" data-target="#SignUpModal">
+				SIGN UP
+				</button>
 				</div>
 			</div>
 			<div class="hero-col2">
@@ -148,8 +159,106 @@ include('connections/connection.php');
 		</div>
 		</section>
 
+	
+
+			<!-- MODAL -->
+		
+			
+
+			<!-- SIGN IN Modal -->
+	<div class="modal fade" id="SignInModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">SIGN IN</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+			<div class="modal-body">
+				<form>
+					<div class="form-group">
+						   <label for="formGroupExampleInput">Username</label>
+ 						   <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+					</div>
+					
+					<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+					<!-- dto mo lagay validate.php -->
+					<button type="button" class="btn btn-primary" >Sign In</button>     
+				</div>
+				</form>
+			</div>
+				</div>
+		</div>
+	</div>
 
 
-	</header>
+
+				<!-- SIGN UP Modal -->
+				<div class="modal fade" id="SignUpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">SIGN UP</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+			<div class="modal-body">
+					<div class="form-group">
+						   <label for="formGroupExampleInput">Username</label>
+ 						   <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+					</div>
+					<div class="form-group">
+						   <label for="formGroupExampleInput">Full Name</label>
+ 						   <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+					</div>
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ex: user@gmail.com">
+				
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+					</div>
+				
+					<div class="form-group">
+					<label for="exampleFormControlSelect1">Barangay</label>
+					<select class="form-control" id="exampleFormControlSelect1">
+						<option>Barangay Nueva</option>
+						<option>Barangay San Roque</option>
+						<option>Barangay Landayan</option>
+						<option>Barangay San Vicente</option>
+						<option>Barangay San Antonio</option>
+					</select>
+					<br>
+					<!-- IMAGE UPLOAD -->
+					<div class="form-group">
+						<label for="exampleFormControlFile1">Upload Profile Photo</label>
+						<input type="file" class="form-control-file" id="exampleFormControlFile1">
+					</div>
+					<form>
+
+					<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+
+					<!-- dto mo lagay yung sa register  -->
+					<button type="button" class="btn btn-primary">Confirm</button>
+				</div>
+				</form>
+			</div>
+				
+				</div>
+		</div>
+	</div>
+	
 </body>
 </html>
