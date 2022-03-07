5cf00e3dc5ff0361324ec958058bc0bf5d3518f4
cielzguerrero <lexiousgz@gmail.com>
2022-03-07 20:28:36 +0800

    plus minus reset points

--- includes/addpoints.php
+++ includes/addpoints.php
@@ -1,29 +1,5 @@
 <?php
-include ('connections/connection.php');
-if (!isset($_GET['ID'])){
 
-    $_SESSION['update'] = "<div class='message warning'>User Not Found.</div>";
-    header("Location: ../main.php?ID={$id}");
-
-} else {
-
-    $id = $_GET['ID'];
-
-    $sql = "SELECT * FROM members WHERE id = $id";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-
-    if($result == TRUE) {
-        $count = mysqli_num_rows($result);
-        
-        if ($count > 0) {
-        } else {
-            $_SESSION['update'] = "<div class='message warning'>User Not Found.</div>";
-            header("Location: ../main.php?ID={$id}");
-        }
-    }
-
-}
 $profileuser = $row['profilename'];
 $activity = $row['profilename']." : Gained 150 points";
 $currentpoint = $row['points'];
@@ -36,8 +12,8 @@ $result = mysqli_query($conn, $sql);
 if($result) {
     $log = "INSERT INTO logs (user, activity) VALUES ('$profileuser', '$activity')";
     $result = mysqli_query($conn, $log);
-    header("Location: ../main.php?ID={$id}");
+    echo "<meta http-equiv='refresh' content='0'>";
 } else {
-    header("Location: ../main.php?ID={$id}");
+    echo "<meta http-equiv='refresh' content='0'>";
 }
 ?>
