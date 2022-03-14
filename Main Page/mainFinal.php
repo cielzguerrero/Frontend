7272a8912ef4a7ad13d31835386e23747f36b5f9
aldrin24 <ramoresaldrinpup@gmail.com>
2022-03-14 20:37:42 +0800

    Main Page

---
+++ Main Page/mainFinal.php
@@ -0,0 +1,407 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+      <!-- OWL CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <link rel="stylesheet" href="main.css">
+   
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/9fb78288eb.js" crossorigin="anonymous"></script>
+    <title>G-Points</title>
+  
+  
+  </head>
+  <body>
+
+<div class="nav-main">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="main-images/Glogo-white.png" alt="" srcset=""></a>
+
+          <!-- HAMBURGER -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <!-- <span class="navbar-toggler-icon navbar-light"></span> -->
+           
+            <span class="clickHamburger">
+                <div class="bar bar1 active"></div>
+                <div class="bar bar2 active"></div>
+                <div class="bar bar3 active"></div>
+            </span>
+          </button>
+
+          <!-- LINKS -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">G-Points</a>
+              </li>
+              
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#news-title">News</a>
+              </li>
+
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#rankings-title">Rankings</a>
+              </li>
+
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#profile-title">Profile</a>
+              </li>
+              
+              <li class="nav-item ">
+                <a class="nav-link active" aria-current="page" href="#logs-title">Logs</a>
+              </li>
+
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                  <img src="main-images/bojji.jpg" alt="">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Profile</a></li>
+                  <li><a class="dropdown-item" href="#">Logs</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Sign Out</a></li>
+                </ul>
+              </li>
+             
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+</div>
+        <!-- WELCOME USER -->
+        <div class="welcome">
+            <h1>Welcome!</h1>
+            <h1>Aldrin Ramores</h1>
+            <p>Start earning points and claim rewards</p>
+        </div>
+        
+       
+
+          <!-- GPOINTS CARD -->
+      <div class="gpoints">
+          <div class="g-points-wrapper">
+            <div class="gpoints-top">
+                <h2>G-C0INS</h2>
+            </div>
+            <div class="gpoints-below">
+              <div class="gpoints-text">
+                <h4>Your Points</h4>
+                <h1>2400</h1>
+              </div>
+              <div class="gpoints-button">
+              <a href="">Claim</a>
+              </div>
+            </div>
+        </div>
+      </div>
+
+
+      <div class="main-wrapper">
+          <!-- NEWS SECTION -->
+           <div class="titles title0" id="news-title">
+            <h2>News</h2>
+           </div>
+          <!-- CAROUSEL -->
+          <div class="owl-carousel owl-theme">
+            <div class="item"><img src="main-images/1img.jpg"><h4>Title</h4><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius sunt vitae eligendi saepe, nihil porro eum inventore perspiciatis ut, suscipit, ipsam dolorem laborum numquam. Aperiam asperiores molestias sunt voluptates fugiat. <a href="">continue reading...</a></p></div>
+            <div class="item"><img src="main-images/2img.jpg"><h4>Title</h4><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius sunt vitae eligendi saepe, nihil porro eum inventore perspiciatis ut, suscipit, ipsam dolorem laborum numquam. Aperiam asperiores molestias sunt voluptates fugiat. <a href="">continue reading...</a></p></div>
+            <div class="item"><img src="main-images/3img.jpg"><h4>Title</h4><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius sunt vitae eligendi saepe, nihil porro eum inventore perspiciatis ut, suscipit, ipsam dolorem laborum numquam. Aperiam asperiores molestias sunt voluptates fugiat. <a href="">continue reading...</a></p></div>
+          </div>
+
+          <!-- RANKING SECTION -->
+           <div class="titles title1" id="rankings-title">
+            <h2>Rankings</h2>
+           </div>
+
+           <div class="rank">
+             <div class="rank-wrapper">
+              <div class="border-color-top"></div>
+          <!-- TOP 3 -->
+               <div class="rank-3">
+                  <div class="top2"><img src="main-images/bojji.jpg"><p>Username</p><h5>2030</h5></div>
+                  <div class="top1"><img src="main-images/bojji.jpg"><p>Username</p><h5>3040</h5></div>
+                  <div class="top3"><img src="main-images/bojji.jpg"><p>Username</p><h5>3000</h5></div>
+               </div>
+               <div class="rank-4-10">
+                  <div class="ranks">
+                      <table class="rank-table">
+                        <tr class = "trBorder">
+                          <td> 4th</td>
+                          <td class="rank-image"><img src="main-images/1img.jpg"></td>
+                          <td>UserName</td>
+                          <td>2030</td>
+                        </tr>
+                        <tr>
+                          <td>5th</td>
+                          <td class="rank-image"><img src="main-images/2img.jpg"></td>
+                          <td>UserName</td>
+                          <td>2030</td>
+                        </tr>
+                        <tr>
+                          <td>6th</td>
+                          <td class="rank-image"><img src="main-images/3img.jpg"></td>
+                          <td>UserName</td>
+                          <td>2030</td>
+                        </tr>
+                        <tr>
+                          <td>7th</td>
+                          <td class="rank-image"><img src="main-images/bojji.jpg"></td>
+                          <td>UserName</td>
+                          <td>2030</td>
+                        </tr>
+                        <tr>
+                          <td>8th</td>
+                          <td class="rank-image"><img src="main-images/bojji.jpg"></td>
+                          <td>UserName</td>
+                          <td>2030</td>
+                        </tr>
+                 
+                        
+                      </table>
+                  </div>
+                 
+                 
+               </div>
+             </div>
+           </div>
+
+
+
+
+          <!-- PROFILE SECTION -->
+           <div class="titles title2" id="profile-title">
+            <h2>Profile</h2>
+           </div>
+           
+
+           <!-- PROFILE IMAGE -->
+        
+
+
+
+           <div class="profile-section">
+             <div class="profile">
+               <div class="border-color-top"></div>
+              <div class="profile-image">
+                <div class="image"><img src="main-images/bojji.jpg" class="ProfileImage"></div><br>
+               </div>
+    
+                <form>
+                  <div class="row">
+                    <div class="col">
+                      <!-- USERNAME -->
+                      <input type="text" class="form-control" placeholder="Username">
+                    </div>
+                    <div class="col">
+                      <!-- FULLNAME -->
+                      <input type="text" class="form-control" placeholder="Full name">
+                    </div>
+                  </div>
+                    <!-- EMAIL -->
+                    <input type="email" class="form-control" placeholder="user@gmail.com">
+                    <!-- PASSWORD -->
+                  <input type="password" class="form-control" placeholder="Password">
+
+                  <!-- BARANGAY -->
+        
+                  <div class="input-group mb-3">
+                  
+                    <select class="custom-select" id="inputGroupSelect01">
+                      <option selected >Barangay Nueva</option>
+                      <option value="1">Barangay San Antonio</option>
+                      <option value="2">Barangay San Vicente</option>
+                      <option value="3">Barangay San Roque</option>
+                      <option value="3">Barangay Landayan</option>
+                   
+                    </select>
+                  </div>
+                  
+                    <!-- SAVE CHANGES -->
+                    <div class="d-flex justify-content-center">
+                      <button type="submit" class="btn btn-outline-primary">Save Changes</button>
+                    </div>
+                 
+                    <!-- HIDDEN FILE INPUT -->
+                    <input type="file" class="custom-file-input UploadImage" id="inputGroupFile02">
+                  
+
+                </form>
+                <div class="border-color-top"></div>
+
+             </div>
+           </div>
+
+
+
+
+          <!-- LOGS SECTION -->
+           <div class="titles title3" id="logs-title">
+            <h2>Logs</h2>
+        </div>
+          <div class="table-wrapper">
+            <table class="table table-bordered">
+              <thead>
+                <tr class ="toplefttable">
+                  <th scope="col">User</th>
+                  <th scope="col">Activity</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Elapsed Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+                <tr>
+                  <th scope="row">User name</th>
+                  <td>Edit Profile</td>
+                  <td>September something</td>
+                  <td>4:00</td>
+                </tr>
+
+            
+          
+              </tbody>
+            </table>
+            
+          </div>
+        <!-- MAIN WRAPPER END -->
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+      <div class="footer-wrapper">
+            <!-- Site footer -->
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <img src="main-images/grewardwhite.png" alt="">
+           
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Quick Link</h6>
+            <ul class="footer-links">
+              <li><a href="#">G-Points</a></li>
+              <li><a href="#news-title">News</a></li>
+              <li><a href="#rankings-title">Rankings</a></li>
+              <li><a href="#profile-title">Profile</a></li>
+              <li><a href="#logs-title">Logs</a></li>
+            
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Information</h6>
+            <ul class="footer-links">
+            
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Getting Started</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Our Team</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by 
+          G-Reward
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-github"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+          </footer> 
+        </div>
+ 
+
+
+    
+      
+     
+
+
+
+
+
+  <!-- bootstrap js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+ <!-- OWL JS -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- MY JS -->
+  <script src="main.js"></script>
+
+
+  
+      
+  </body>
+</html>
