2d6ec41e7a1fb427c7c88f8d1384e6c8f9615baa
aldrin24 <ramoresaldrinpup@gmail.com>
2022-03-16 07:30:16 +0800

    updates

--- mainPage/GettingStarted.php
+++ mainPage/GettingStarted.php
@@ -25,7 +25,7 @@
 <div class="nav-main">
     <nav class="navbar navbar-expand-lg navbar-light ">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="main-images/Glogo-white.png" alt="" srcset=""></a>
+          <a class="navbar-brand" href="#"><img src="main-images/Glogo-white.png" ></a>
 
           <!-- HAMBURGER -->
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -40,7 +40,7 @@
 
           <!-- LINKS -->
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+            <ul class="navbar-nav ms-auto mb-2  align-items-center">
                 
             <li class="nav-item ">
                 <a class="nav-link active" aria-current="page" href="mainFinal.php">Home</a>
@@ -73,51 +73,15 @@
 
 <!-- CONTENT -->
    
-    <section class="title-card" id="steps">
-                <div class="title-card-wrapper">
-                    <div><img src="../img/Procedures.gif" class="title-card-img" id = "img"></div>
-                    <div class="title-card-text">
-                        <h1>GETTING STARTED</h1>
-                        <h3>Follow This Easy Steps To Claim Your Points. Prizes May Vary Monthly</h3>
-                    </div>
-                </div>
-            </section>
-
-
-
-		<section class = "card-content">
-			<div class="card-content-box">
-				<div class="card-1 card-box">
-					<img src="../img/signup.png" alt="" srcset="">
-					<h3>REGISTER</h3>
-					<p>Create an account before segregating your non profitable and profitable waste</p>
-				</div>
-				<div class="card-2 card-box">
-				<img src="../img/login.png" alt="" srcset="">
-					<h3>LOG IN</h3>
-					<p>Log your account to your respective barangay for identification</p>
-				</div>
-				<div class="card-3 card-box" >
-					<img src="../img/throw.png" alt="" srcset="">
-					<h3>DROP</h3>
-					<p>Once the system recognized your identity, you can now drop all your trash in the bins.</p>
-				</div>
-                <br>
-                <br>
-                <br>
-                
-			</div>
-    </div>
     
+
+
 
 
 
   <!-- bootstrap js -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
- <!-- OWL JS -->
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <!-- MY JS -->
   <script src="main.js"></script>
 
