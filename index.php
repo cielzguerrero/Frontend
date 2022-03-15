1fc34a9ea45d48c2209db250bfca64cee5b17241
aldrin24 <ramoresaldrinpup@gmail.com>
2022-03-16 03:50:12 +0800

    index update ui

--- index.php
+++ index.php
@@ -48,30 +48,33 @@ include('includes/register.php');
 			</div>
 		</div>
 		</nav>  -->
-	
+		<div class="top">
+		
 		<nav class="navbar navbar-expand-lg navbar-light">
-  <img src="img/Glogo-white.png">
-  <button class="navbar-toggler "id = "nav-toggle-button" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span><i class="fa-solid fa-bars fa-lg "></i></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav ms-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Getting Started <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-	  <li class="nav-item">
-        <a class="nav-link" href="#">Our Team</a>
-      </li>
-   
-    </ul>
-  </div>
-</nav>
+			<img src="img/Glogo-white.png">
+			<button class="navbar-toggler "id = "nav-toggle-button" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span><i class="fa-solid fa-bars fa-lg "></i></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav ms-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Getting Started <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Our Team</a>
+				</li>
+			
+				</ul>
+			</div>
+			</nav>
+		</div>
+		
 	
 
 	<header>
