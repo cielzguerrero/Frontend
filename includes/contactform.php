e390122ebd173d36912c23d58b491db870b56443
cielzguerrero <lexiousgz@gmail.com>
2022-03-29 14:46:12 +0800

    adad

--- includes/contactform.php
+++ includes/contactform.php
@@ -4,7 +4,9 @@ require('../includes/PHPMailer-master/src/SMTP.php');
 require('../includes/PHPMailer-master/src/Exception.php');
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\OAuth;
 use PHPMailer\PHPMailer\Exception;
+use League\OAuth2\Client\Provider\Google;
     if(isset($_POST["submit"])){
         $name = $_POST["name"];
         $subject = $_POST["subject"];
@@ -12,22 +14,32 @@ use PHPMailer\PHPMailer\Exception;
         $message = $_POST["message"];
 
         $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = "smtp.gmail.com";
-        $mail->SMTPAuth = "true";
-        $mail->SMTPSecure = "tls";
-        $mail->Port = "587";
-        $mail->Username = "grewardsmanagement@gmail.com"; //ito yung gamit kong email pang send
-        $mail->Password = "grewardsmanagement123!"; //password ng email ko
-        $mail->Subject = $subject; //subject ng email natin
-        $mail->setFrom($mailFrom,"From : $name <$mailFrom> "); //kung kanino galing yung email
-        $mail->isHTML(true); //naka true para madesignan yung email body
-        $mail->Body = $message; // dito sa body pwede mo designan ng html at css yung body ng email natin
-        $mail->addAddress("grewardsmanagement@gmail.com"); //kung kanino isesend yung email, wala nang problema dito
+        $mail->Host     = "smtp1.example.com;smtp2.example.com";
+        $mail->Mailer   = "smtp";
+        // $mail->isSMTP();
+        // $mail->Host = 'smtp.gmail.com';
+        // $mail->SMTPAuth = "true";
+        // $mail->SMTPSecure = "tls";
+        // $mail->AuthType = "XOAUTH2";
+        // $mail->Port = "587";
+        // $mail->Subject = $subject; //subject ng email natin
+        // $mail->setFrom($mailFrom, "From : $name"); //kung kanino galing yung email
+        // $mail->isHTML(true); //naka true para madesignan yung email body
+        // $mail->Body = $message; // dito sa body pwede mo designan ng html at css yung body ng email natin
+        // $mail->addAddress("grewardmanagement@gmail.com"); //kung kanino isesend yung email, wala nang problema dito
+        // $mail->addReplyTo($mailFrom, "Reply");
+
+        $mail->From = $mailFrom;
+        $mail->FromName = $name;
+        $mail->addAddress("grewardmanagement@gmail.com", "John Doe");     // Add a recipient
+
+        $mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = $subject;
+        $mail->Body = $message;
 
                 if ($mail->Send()) {
                     echo "<meta http-equiv='refresh' content='0'>";
                 }
-                $mail->smtpClose();
     }
 ?>
