d55b8f3c9cddb1bd8c7de501f1b46a800f7d752d
aldrin ramores <ramoresaldrinpup@gmail.com>
2022-05-03 07:50:10 +0800

    asd

---
+++ AdminTailWind/dashboard.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- TAILWIND CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body  class ="bg-red-200">
+
+<!-- SIDEBAR -->
+  <section class = "side fixed max-w-4xl">
+      <div class="sidebar bg-white">
+            <div class="sidebar__logo">
+                <h1 class= "text-4xl">Waste Management </h1>
+            </div>
+      </div>
+  </section>
+        
+
+
+</body> 
+</html> 
